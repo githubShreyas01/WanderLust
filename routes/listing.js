9a6ed0cef2badf9d83f083f6f010a2ea6d43619b
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-10-14 11:21:59 +0530

    Add files via upload

--- routes/listing.js
+++ routes/listing.js
@@ -4,51 +4,23 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
+const listingController = require("../controllers/listings.js");
 
-//Index Route
-router.get("/",  wrapAsync(async (req, res) =>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-}));
+router.route("/")
+.get(wrapAsync(listingController.index)) //Index Route
+.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); //Create Route
 
 // New Route
-router.get("/new",isLoggedIn,  (req, res) =>{
-    res.render("listings/new.ejs");
-});
+router.get("/new",isLoggedIn,  listingController.renderNewForm );
 
-// Show Route
-router.get("/:id", wrapAsync(async (req, res) =>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate({path: "reviews", populate: {
-        path: "author",// we want with every listing reviews get printed along with every review its author also gets printed  
-        }
-    })
-    .populate("owner"); // populate used to show data of reviews array
-    if(!listing){
-        req.flash("error", "Listing you requested for does not exist !");
-        res.redirect("/listings");
-    }
-    console.log(listing);
-    res.render("listings/show.ejs", {listing});
-}));
+router.route("/:id")
+.get( wrapAsync(listingController.showListing)) // Show Route
+.put(isLoggedIn,isOwner, wrapAsync(listingController.updateListing)) //Update Route
+.delete(isLoggedIn,isOwner,  wrapAsync(listingController.destroyListing)); //Delete Route
 
- //Create Route
-router.post("/",isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
-    console.log(req.body); // Log the request body
-    const { listing } = req.body;
-    const newListing = new Listing(listing);
-    newListing.owner = req.user._id;
-    const { url } = req.body.listing.image;
-    const filename = "listingimage"; // or however you want to handle the filename
-    if (typeof url === 'string') {
-        newListing.image = { url, filename };
-    } else {
-        throw new Error('Invalid image URL');
-    }
-    await newListing.save();
-    req.flash("success", "New Listing Created!");
-    res.redirect("/listings");
-}));
+
+
+
 
 // old Create Route
 // router.post("/",  wrapAsync(async (req, res, next) =>{
@@ -63,31 +35,8 @@ router.post("/",isLoggedIn, validateListing, wrapAsync(async (req, res, next) =>
 // );
 
 //Edit Route
-router.get("/:id/edit",isLoggedIn,isOwner,  wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing){
-        req.flash("error", "Listing you requested for does not exist !");
-        res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs", {listing});
-}));
+router.get("/:id/edit",isLoggedIn,isOwner,  wrapAsync(listingController.renderEditForm));
 
-//Update Route
-router.put("/:id",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
-    console.log(req.body); // Log the request body
-    const { id } = req.params;
-    const { listing } = req.body;
-    const updatedListing = await Listing.findByIdAndUpdate(id, listing, { new: true });
-
-    if (req.body.listing.image && req.body.listing.image.url) {
-        updatedListing.image.url = req.body.listing.image.url;
-    }
-
-    await updatedListing.save();
-    req.flash("success", "Listing Updated!");
-    res.redirect(`/listings/${id}`);
-}));
 
 // old Update Route
 // router.put("/:id", wrapAsync(async (req, res) =>{
@@ -98,15 +47,5 @@ router.put("/:id",isLoggedIn,isOwner, wrapAsync(async (req, res) => {
 //         await listing.save();
 //     res.redirect(`/listings/${id}`);
 // }));
-
-//Delete Route
-router.delete("/:id",isLoggedIn,isOwner,  wrapAsync(async (req, res) =>{
-    let {id} = req.params;
-    let deletedListing =  await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    req.flash("success", "Listing Deleted!");
-    res.redirect("/listings");
-}));
-
 
 module.exports = router;
